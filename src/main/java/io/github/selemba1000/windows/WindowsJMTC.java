351b9d4a333ba41fc3bd8f057aa1e870cb80230b
GaodaGG <gaodagg50@gmail.com>
2025-10-27 16:19:48 +0800

    Refactor music album properties handling in WindowsJMTC.java

--- src/main/java/io/github/selemba1000/windows/WindowsJMTC.java
+++ src/main/java/io/github/selemba1000/windows/WindowsJMTC.java
@@ -232,10 +232,8 @@ public class WindowsJMTC extends JMTC {
             JMTCMusicProperties mediaPropertiesCast = (JMTCMusicProperties) mediaProperties;
             lib.setMusicTitle(new WString(mediaPropertiesCast.title));
             lib.setMusicArtist(new WString(mediaPropertiesCast.artist));
-            if (!mediaPropertiesCast.albumTitle.isEmpty())
-                lib.setMusicAlbumTitle(new WString(mediaPropertiesCast.albumTitle));
-            if (!mediaPropertiesCast.albumArtist.isEmpty())
-                lib.setMusicAlbumArtist(new WString(mediaPropertiesCast.albumArtist));
+            lib.setMusicAlbumTitle(new WString(mediaPropertiesCast.albumTitle));
+            lib.setMusicAlbumArtist(new WString(mediaPropertiesCast.albumArtist));
             if (mediaPropertiesCast.albumTracks >= 0)
                 lib.setMusicAlbumTrackCount(new UnsignedInt(mediaPropertiesCast.albumTracks));
             if (mediaPropertiesCast.track >= 0) lib.setMusicTrack(new UnsignedInt(mediaPropertiesCast.track));
